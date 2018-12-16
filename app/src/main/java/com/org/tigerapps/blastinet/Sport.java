6334f032e209a9995506be6a99d1a1b01b3b2c98
RayMiles94 <natsuboy94@gmail.com>
2018-12-16 01:15:48 +0100

    change 18

--- app/src/main/java/com/org/tigerapps/blastinet/Sport.java
+++ app/src/main/java/com/org/tigerapps/blastinet/Sport.java
@@ -17,10 +17,20 @@ public class Sport extends AppCompatActivity {
         adpater1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         sp1.setAdapter(adpater1);
 
-        Spinner sp2 = (Spinner) findViewById(R.id.spin4);
-        ArrayAdapter<String> adpater2 = new ArrayAdapter<String>(Sport.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.spin2));
+        Spinner sp6 = (Spinner) findViewById(R.id.spin6);
+        ArrayAdapter<String> adpater2 = new ArrayAdapter<String>(Sport.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.years));
         adpater2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp2.setAdapter(adpater2);
+        sp6.setAdapter(adpater2);
+
+        Spinner sp7 = (Spinner) findViewById(R.id.spin7);
+        ArrayAdapter<String> adpater3 = new ArrayAdapter<String>(Sport.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.months));
+        adpater2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp7.setAdapter(adpater3);
+
+        Spinner sp8 = (Spinner) findViewById(R.id.spin8);
+        ArrayAdapter<String> adpater4 = new ArrayAdapter<String>(Sport.this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.days));
+        adpater2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp8.setAdapter(adpater4);
 
     }
 }
