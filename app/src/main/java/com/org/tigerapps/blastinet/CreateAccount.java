d266f90802494c28d2f2fc4d4d9027f0d2a40d39
Unholyhoper <haithemby@gmail.com>
2018-12-16 08:54:48 +0100

    change 32

--- app/src/main/java/com/org/tigerapps/blastinet/CreateAccount.java
+++ app/src/main/java/com/org/tigerapps/blastinet/CreateAccount.java
@@ -3,9 +3,15 @@ package com.org.tigerapps.blastinet;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
 
 public class CreateAccount extends AppCompatActivity {
 
+
+    EditText mail;
+    EditText name;
+    EditText lname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,7 +19,13 @@ public class CreateAccount extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
+        mail = (EditText)  findViewById(R.id.mail);
+        name = (EditText)  findViewById(R.id.name);
+        lname = (EditText)  findViewById(R.id.lname);
     }
 
+
+    public void SaveInDB(View v){
+            
+    }
 }
