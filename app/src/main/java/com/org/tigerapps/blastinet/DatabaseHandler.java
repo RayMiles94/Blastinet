dcef77ec5c4ac968de52e3e0dc89e137930d14b9
Unholyhoper <haithemby@gmail.com>
2018-12-16 01:33:39 +0100

    Fixed theatre

--- app/src/main/java/com/org/tigerapps/blastinet/DatabaseHandler.java
+++ app/src/main/java/com/org/tigerapps/blastinet/DatabaseHandler.java
@@ -1,4 +1,40 @@
 package com.org.tigerapps.blastinet;
 
-public class DatabaseHandler {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper {
+    public static final String BLASTI_CLIENT_KEY="id";
+    public static final String BLASTI_NAME="name";
+    public static final String BLASTI_EMAIL="email";
+    public static final String BLASTI_EVENTS="events";
+
+    public static final String BLASTI_TABLE_NAME = "Client";
+
+    public static final String BLASTI_TABLE_CREATE=
+            "CREATE TABLE" + BLASTI_TABLE_NAME + "(" + BLASTI_CLIENT_KEY + "INTEGER PRIMARY KEY AUTOINCREMENT " +
+            BLASTI_NAME + "TEXT" +
+            BLASTI_EMAIL + "TEXT" +
+            BLASTI_EVENTS + "TEXT NOT NULL);";
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+
+        super(context, name, factory, version);
+
+    }
+    @Override
+
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(BLASTI_TABLE_CREATE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
 }
